a4b10520a9789e3df29304d07c13b84b5b6689b0
Karen <karenachoyan8@gmail.com>
2025-04-04 23:42:32 +0400

    Changes news

--- components/news/details.jsx
+++ components/news/details.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ItemNews from "@/components/news/itemNews";
 import {useApp} from "@/providers/AppProvider";
+import parse from "html-react-parser";
 
 const Details = ({news, lastNews}) => {
     const {lang} = useApp();
@@ -15,13 +16,13 @@ const Details = ({news, lastNews}) => {
             <div className='w-full bg-[#232222] pt-[160px]'>
                 <div className='container m-auto bg-[#4D4C4C] rounded-3xl'>
                     <div className='w-[80%] m-auto pb-[50px]'>
-                        <h1 className='text-center my-5 text-[45px] text-[#C7C7C7]'>News</h1>
+                        <h1 className='text-center my-5 pt-10 text-[45px] text-[#C7C7C7]'>News</h1>
                         <img  className='w-full' src={process.env.IMAGE_URL+news.avatar} alt={"News"}/>
                         <div className='w-[50%] py-10'>
                             <h1 className='text-[23px] sm:text-[30px] text-white font-bold'>{tit}</h1>
                         </div>
                         <div className='text-[20px] text-white '>
-
+                            {parse(content)}
                         </div>
                     </div>
 
