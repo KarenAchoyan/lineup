09f12977ee1e07a79f98d3e3bdcd27ae4dfda14f
Karen <karenachoyan8@gmail.com>
2025-04-15 15:31:37 +0400

    changes

--- app/[lang]/forMash/page.jsx
+++ app/[lang]/forMash/page.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Page = () => {
     return (
         <div className='w-full bg-[#232222] pt-[160px] '>
-            <div className="container m-auto bg-[#4D4C4C] rounded-3xl mx-auto px-4 py-10 ">
+            <div className="container m-auto bg-[#4D4C4C] rounded-3xl mx-auto py-10 ">
                 <h1 className='text-center my-5 text-[32px]  md:text-[45px] text-[#C7C7C7]'>Մաշիս համար</h1>
-                <div className='text-[20px] text-white my-10 text-center px-30'>
+                <div className='text-[20px] text-white my-10 text-center px-4 sm:px-30'>
                     Մաաշս, ես գիտեմ որ քեզ նեղացրել եմ, գիտեմ որ հիմա դու տխուր ես, գիտեմ որ հիմա քո մոտ հոգիդ
                     խառնաշփոթա, բայց գիտես, ես ամեն ինչ ուզում եմ անեմ որ դու գոնե միքիչ ժպտաս։
                     Մաշս դու պտի միշտ ժպտաս, որովհետև դու ամենահամով, ամենասիրուն ամենապելոք, ամենաաաա ամենա աղջիկս ես
