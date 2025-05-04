2bc2776d0fe144c5887af007e6b6b281c0a5b955
Karen <karenachoyan8@gmail.com>
2025-05-04 23:08:59 +0400

    Changes

--- components/activeUsers/details.jsx
+++ components/activeUsers/details.jsx
@@ -7,31 +7,37 @@ const Details = ({params, data, dict}) => {
     const content = (params.lang.toLowerCase() === "hy") ? data?.content_hy : params.lang.toLowerCase() === "ge" ? data?.content_ge : params.lang.toLowerCase() === "ru" ? data?.content_ru : data?.content_en;
     return (
         <>
-            <div className='container m-auto bg-[#4D4C4C] rounded-3xl pt-20'>
+            <div className='container m-auto bg-[#4D4C4C] min-h-[100vh] rounded-3xl pt-20'>
                 <h1 className='text-center my-5 text-[32px]  md:text-[45px] text-[#C7C7C7] pb-10'>{dict.active_youth}</h1>
                 <div className='w-[80%] m-auto pb-[50px]'>
                     <div className='text-center md:text-left'>
                         <div className='md:float-left mx-auto w-[300px] '>
-                            <Image src={process.env.IMAGE_URL + data.avatar} width={250} height={250}
+                            {data?.avatar && (
+                            <Image src={process.env.IMAGE_URL + data?.avatar} width={250} height={250}
                                    className='rounded-full float-left m-3 mr-5 object-cover'
                                    alt={"Avatar staff"}/>
+                            )}      
                         </div>
                         <h2 className='text-[45px] text-[#F15A2B] font-bold'>{title}</h2>
                         <div className='text-[20px] text-white'>
-                            {parse(content)}
+                            {content && parse(content)}
                         </div>
                     </div>
                     <div className='flex justify-between flex-wrap'>
                         <div className='w-full sm:w-[48%] mt-10'>
+                            {data?.images && (
                             <Image src={process.env.IMAGE_URL + JSON.parse(data.images)[0]} alt={"Image"}
                                    className='w-full  rounded-2xl'/>
+                            )}
                         </div>
                         <div className='w-full sm:w-[48%]'>
-                            {JSON.parse(data.images).slice(1).map((item) => (
+                            {data?.images && (
+                            JSON.parse(data.images).slice(1).map((item) => (
                                 <div className='w-full md:h-[300px] mt-10 rounded-2xl overflow-hidden'>
                                     <Image src={process.env.IMAGE_URL+item} alt={"Image"} className='h-full w-full object-cover'/>
                                 </div>
-                            ))}
+                            ))
+                            )}
                         </div>
                     </div>
                 </div>
